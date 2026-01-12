2ce87e85688733c37664cc77d9a2fff2765c0cb9
Harsha vardhini Mergu <100754463+Harshamergu@users.noreply.github.com>
2026-01-13 00:18:48 +0530

    Refactor EC2 instance definitions in main.tf
    Updated instance definitions to use new naming convention and removed unused instances.

--- tf-ansible-stack-dependencies/tf/main.tf
+++ tf-ansible-stack-dependencies/tf/main.tf
@@ -24,22 +24,13 @@ data "aws_ami" "ubuntu" {
 
 locals {
   instances = {
-    instance1 = {
+    ec2-instance-1 = {
       ami           = data.aws_ami.ubuntu.id
       instance_type = "t2.micro"
     }
-    instance2 = {
+    ec2-instance-2 = {
       ami           = data.aws_ami.ubuntu.id
-      instance_type = "t2.micro"
-    }
-    instance3 = {
-      ami           = data.aws_ami.ubuntu.id
-      instance_type = "t2.micro"
-    }
-    instance4 = {
-      ami           = data.aws_ami.ubuntu.id
-      instance_type = "t2.micro"
-    }
+      instance_type = "t2.micro"  
   }
 }
 
