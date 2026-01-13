512f021010e885075620c952513b9bc520b14cb7
Harsha vardhini Mergu <100754463+Harshamergu@users.noreply.github.com>
2026-01-13 13:20:27 +0530

    Comment out aws_instances output and add instances output

--- tf-ansible-stack-dependencies/tf/outputs.tf
+++ tf-ansible-stack-dependencies/tf/outputs.tf
@@ -1,3 +1,8 @@
-output "aws_instances" {
-  value = [for instance in aws_instance.this : instance.public_ip]
+#output "aws_instances" {
+# value = [for instance in aws_instance.this : instance.public_ip]
+#}
+
+output "instances" {
+  description = "Public IPs of EC2 instances"
+  value       = [for instance in aws_instance.this : instance.public_ip]
 }
