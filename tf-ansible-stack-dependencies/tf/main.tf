f7a30de7341cc0e9d3a0d7b08df6e4a6f1bd7ca1
Harsha vardhini Mergu <100754463+Harshamergu@users.noreply.github.com>
2026-01-13 12:08:17 +0530

    Rename EC2 instances to virtual computers

--- tf-ansible-stack-dependencies/tf/main.tf
+++ tf-ansible-stack-dependencies/tf/main.tf
@@ -24,13 +24,17 @@ data "aws_ami" "ubuntu" {
 
 locals {
   instances = {
-    ec2-instance-1 = {
+    virtual-computer-1 = {
       ami           = data.aws_ami.ubuntu.id
       instance_type = "t2.micro"
     }
-    ec2-instance-2 = {
+    virtual-computer-2 = {
       ami           = data.aws_ami.ubuntu.id
       instance_type = "t2.micro"  
+    }
+    virtual-computer-3 = {
+      ami           = data.aws_ami.ubuntu.id
+      instance_type = "t2.micro"
     }
   }
 }
