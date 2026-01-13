776d3343309fd2d3c2ab8c4554333e2acdae4287
Harsha vardhini Mergu <100754463+Harshamergu@users.noreply.github.com>
2026-01-13 21:14:51 +0530

    Add configuration for virtual-4 instance

--- tf-ansible-stack-dependencies/tf/main.tf
+++ tf-ansible-stack-dependencies/tf/main.tf
@@ -36,6 +36,10 @@ locals {
       ami           = data.aws_ami.ubuntu.id
       instance_type = "t2.micro"  
     }
+    virtual-4 = {
+      ami           = data.aws_ami.ubuntu.id
+      instance_type = "t2.micro"  
+    }
   }
 }
 
