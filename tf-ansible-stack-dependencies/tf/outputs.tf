cdea62917a7be4fdc0c44c919b7f02924e7332de
Harsha vardhini Mergu <100754463+Harshamergu@users.noreply.github.com>
2026-01-13 22:09:02 +0530

    Refactor output definitions in outputs.tf

--- tf-ansible-stack-dependencies/tf/outputs.tf
+++ tf-ansible-stack-dependencies/tf/outputs.tf
@@ -1,10 +1,4 @@
-#output "aws_instances" {
- # value = [for instance in aws_instance.this : instance.public_ip]
-#}
-
-output "instances" {
-  value = {
-    for k, v in aws_instance.this :
-    k => v.public_ip
-  }
+output "aws_instances" {
+  value = [for instance in aws_instance.this : instance.public_ip]
 }
+
