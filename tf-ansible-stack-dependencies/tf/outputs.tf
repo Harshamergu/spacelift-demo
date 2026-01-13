0365de29e4660cbd3ec892bd1022a8eae5e426a9
Harsha vardhini Mergu <100754463+Harshamergu@users.noreply.github.com>
2026-01-13 16:27:14 +0530

    Refactor output for public IPs of EC2 instances

--- tf-ansible-stack-dependencies/tf/outputs.tf
+++ tf-ansible-stack-dependencies/tf/outputs.tf
@@ -1,8 +1,3 @@
-#output "aws_instances" {
-# value = [for instance in aws_instance.this : instance.public_ip]
-#}
-
-output "instances" {
-  description = "Public IPs of EC2 instances"
-  value       = [for instance in aws_instance.this : instance.public_ip]
+output "public_ips" {
+  value = [for i in aws_instance.this : i.public_ip]
 }
