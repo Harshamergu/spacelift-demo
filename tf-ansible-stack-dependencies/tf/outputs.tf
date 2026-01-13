a5746268fe1586d4ec9051be6bb0c2cb839939c1
Harsha vardhini Mergu <100754463+Harshamergu@users.noreply.github.com>
2026-01-13 21:57:00 +0530

    Refactor outputs in outputs.tf
    Comment out the aws_instances output and add instances output.

--- tf-ansible-stack-dependencies/tf/outputs.tf
+++ tf-ansible-stack-dependencies/tf/outputs.tf
@@ -1,3 +1,10 @@
-output "aws_instances" {
-  value = [for instance in aws_instance.this : instance.public_ip]
+#output "aws_instances" {
+ # value = [for instance in aws_instance.this : instance.public_ip]
+#}
+
+output "instances" {
+  value = {
+    for k, v in aws_instance.this :
+    k => v.public_ip
+  }
 }
